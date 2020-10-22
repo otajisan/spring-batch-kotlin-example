408d158ee80f481bdb9445171b982568cb74b915
otajisan <taji.taji.otajisan@gmail.com>
2020-10-22 14:19:50 +0900

    :+1: add exit status

--- src/main/kotlin/com/example/BatchApplication.kt
+++ src/main/kotlin/com/example/BatchApplication.kt
@@ -1,11 +1,13 @@
 package com.example
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import kotlin.system.exitProcess
 
 @SpringBootApplication
 class BatchApplication
 
 fun main(args: Array<String>) {
-    runApplication<BatchApplication>(*args)
+    exitProcess(SpringApplication.exit(runApplication<BatchApplication>(*args)))
 }
